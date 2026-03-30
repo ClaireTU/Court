40326f39a432d6ce113fa79e5e87c7eed1fdb179
ClaireTU <52907846+ClaireTU@users.noreply.github.com>
2026-03-30 13:31:59 +0800

    Add service worker for caching and fetch handling

---
+++ sw.js
@@ -0,0 +1,34 @@
+const CACHE_NAME = 'gavel-cache-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icons/favicon.svg',
+  './icons/icon-192.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.filter(name => name !== CACHE_NAME).map(name => caches.delete(name))
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    }).catch(() => caches.match('./index.html'))
+  );
+});
